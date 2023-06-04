b0bf3864f1cac6f24622420e9ef0ae24de2307dc
rthoth! <ronaldo.asilva@gmail.com>
2023-06-04 11:58:29 -0300

    chore: add `type` in HotelmError for user errors.

--- src/main/scala/hotelm/handler/protocol/HotelmError.scala
+++ src/main/scala/hotelm/handler/protocol/HotelmError.scala
@@ -3,8 +3,10 @@ package hotelm.handler.protocol
 import zio.json.DeriveJsonEncoder
 import zio.json.JsonEncoder
 
-final case class HotelmError(message: String)
+final case class HotelmError(message: String, `type`: Option[String] = None)
 
 object HotelmError:
 
-  given JsonEncoder[HotelmError] = DeriveJsonEncoder.gen
+  def apply(throwable: Throwable): HotelmError =
+    new HotelmError(throwable.getMessage, Some(throwable.getClass.getSimpleName))
+  given JsonEncoder[HotelmError]               = DeriveJsonEncoder.gen
